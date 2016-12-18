b9d3f49ede90c3bbec81220f5e7407c5684dc451
Josephina Bos <josephinabos@gmail.com>
2016-12-18 11:42:53 +0100

    add metatags

--- config.rb
+++ config.rb
@@ -58,6 +58,7 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
+
   # For example, change the Compass output style for deployment
   # activate :minify_css
 
--- source/javascripts/navbar.js
+++ source/javascripts/navbar.js
@@ -2,14 +2,58 @@
 
 //   $(window).scroll(function () {
 //       console.log($(window).scrollTop())
-//     if ($(window).scrollTop() < 350) {
-//       $('.navbar').addClass('navbar-fixed-left');
+//     if ($(window).scrollTop() < 685) {
+//       $('.navbar').addClass('navbar-fixed-top');
 //     }
-//     if ($(window).scrollTop() > 351) {
-//       $('.navbar').removeClass('navbar-fixed-left');
-//       $('.navbar').addClass('navbar-fixed-right');
+//     if ($(window).scrollTop() > 685) {
+//       $('.navbar').removeClass('navbar-fixed-top');
+//       $('.navbar').addClass('navbar-fixed-about');
+//     }
+//     if ($(window).scrollTop() > 1380) {
+//       $('.navbar').removeClass('navbar-fixed-about');
+//       $('.navbar').addClass('navbar-fixed-resume');
+//     }
+//     if ($(window).scrollTop() > 1800) {
+//       $('.navbar').removeClass('navbar-fixed-resume');
+//       $('.navbar').addClass('navbar-fixed-contact');
 //     }
 //   });
+// });
+
+// var lastScrollTop = 0;
+// $(window).scroll(function(event){
+//    var st = $(this).scrollTop();
+//    if (st > lastScrollTop){
+//       if ($(window).scrollTop() < 685) {
+//       $('.navbar').addClass('navbar-fixed-top-down');
+//       }
+//       if ($(window).scrollTop() > 685) {
+//         $('.navbar').removeClass('navbar-fixed-top-down');
+//         $('.navbar').addClass('navbar-fixed-about-down');
+//       }
+//       if ($(window).scrollTop() > 1380) {
+//         $('.navbar').removeClass('navbar-fixed-about-down');
+//         $('.navbar').addClass('navbar-fixed-resume-down');
+//       }
+//       if ($(window).scrollTop() > 1800) {
+//         $('.navbar').removeClass('navbar-fixed-resume-down');
+//         $('.navbar').addClass('navbar-fixed-contact-down');
+//       }
+//     } else {
+//         if ($(window).scrollTop() > 685) {
+//           $('.navbar').removeClass('navbar-fixed-about-up');
+//           $('.navbar').addClass('navbar-fixed-top-up');
+//         }
+//         if ($(window).scrollTop() > 850) {
+//           $('.navbar').removeClass('navbar-fixed-resume-up');
+//           $('.navbar').addClass('navbar-fixed-about-up');
+//         }
+//         if ($(window).scrollTop() < 1000) {
+//           $('.navbar').removeClass('navbar-fixed-contact-down');
+//           $('.navbar').addClass('navbar-fixed-resume-up');
+//         }
+//   }
+//   lastScrollTop = st;
 // });
 
 $(document).ready(function() {
